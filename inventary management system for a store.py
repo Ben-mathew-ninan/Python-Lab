94d68ebcc378b5d283bb73e773e7487095c7de54
Ben-mathew-ninan <benninan770@gmail.com>
2024-11-19 16:28:24 +0530

    inventory management

---
+++ inventary management system for a store.py
@@ -0,0 +1,28 @@
+'''Author=Ben Mathew Ninan
+Date=19/11/2024
+Python program To Find
+inventary management system
+'''
+
+
+inventory=[
+    ("Laptop",5,30000.00),
+    ("Headphones",15,500.00),
+    ("Mouse",50,150.00),
+    ("Keyboard",20,150.00),
+    ("Monitor",10,3000.00),
+]
+highest_stock_value=0
+item_with_highest_stock_value=None
+for item in inventory:
+    item_name,quantity,unit_price=item
+    stock_value=quantity * unit_price
+    print(f"Item Name:{item_name},the Stock value is: {stock_value}")
+    if stock_value>highest_stock_value:
+        highest_stock_value=stock_value
+        item_with_highest_stock_value= item_name
+print(f"highest stock value is :{highest_stock_value}")
+print(f"item with highest stock value is : {item_with_highest_stock_value}")
+
+
+
