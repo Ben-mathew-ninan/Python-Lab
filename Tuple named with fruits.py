2e24da2b645bb691d0c81eaf319182612a9e1226
Ben-mathew-ninan <benninan770@gmail.com>
2024-11-19 16:32:00 +0530

    tuple program

---
+++ Tuple named with fruits.py
@@ -0,0 +1,22 @@
+'''
+Author=Ben Mathew Ninan
+Date=19/11/2024
+Python program
+tuple
+'''
+
+fruits=("apple","banana","cherry","orange")
+print(fruits[1])
+print(fruits[-1])
+print(fruits[-2])
+
+
+even_numbers=(2,4,6)
+odd_numbers=(1,3,5)
+numbers=even_numbers+odd_numbers
+print(numbers)
+repeat_tuple=("hello",)+3
+print(repeat_tuple)
+
+
+person=("Alice",25,"Engineer")
